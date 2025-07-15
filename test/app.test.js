c44ae79bd7dc37140204372695106165be4886fb
ManeeshaDosen <maneesha@dosen.io>
2025-07-15 11:40:31 +0530

    Add dummy test to trigger GPT explanation

---
+++ test/app.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest')
+const app = require('../app.js')
+
+describe('GET /', ()=>{
+    it('should return message from Maneesha', (done)=>{
+        request(app).get('/').expect(200).expect((res)=>{
+            if(!res.text.toLowerCase().trim().includes('maneesha')){
+                throw new Error("Response doesn't contain expected text maneesha")
+            }
+        }).end(done)
+    })
+    it('should fail intentionally for testing GPT', (done) => {
+    request(app)
+      .get('/non-existent-route')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+})
